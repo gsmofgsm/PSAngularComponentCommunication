98ded310e70bedbeb14cd6a10b86c3efa523abb0
Qing Ma <qing.ma@isatis-health.nl>
2019-11-03 21:58:36 +0100

    productShellComponent also subscribes to selectedProductChanges$

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
     templateUrl: './product-shell.component.html'
@@ -7,9 +8,19 @@ export class ProductShellComponent implements OnInit {
     pageTitle: string = 'Products';
     monthCount: number;
 
-    constructor() { }
+    constructor(private productService: ProductService) { }
 
     ngOnInit() {
+        this.productService.selectedProductChanges$.subscribe(selectedProduct => {
+            if (selectedProduct) {
+                const start = new Date(selectedProduct.releaseDate);
+                const now = new Date();
+                this.monthCount = start.getMonth() - now.getMonth()
+                    + 12 * (start.getFullYear() - now.getFullYear());
+            } else {
+                this.monthCount = 0;
+            }
+        });
     }
 
 }
